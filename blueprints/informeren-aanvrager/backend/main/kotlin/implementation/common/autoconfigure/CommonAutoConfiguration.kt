836a0821b1b932f4fe213c8aa22312218da834ed
lrnspprs <laurens.piepers@ritense.com>
2023-06-06 15:18:02 +0200

    removed DocumentReaderService.kt fixed mentions and faulty expressions

--- blueprints/informeren-aanvrager/backend/main/kotlin/implementation/common/autoconfigure/CommonAutoConfiguration.kt
+++ blueprints/informeren-aanvrager/backend/main/kotlin/implementation/common/autoconfigure/CommonAutoConfiguration.kt
@@ -1,8 +1,6 @@
 package implementation.common.autoconfigure
 
-import com.ritense.document.service.DocumentService
 import implementation.common.service.DateTimeService
-import implementation.common.service.DocumentReaderService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
